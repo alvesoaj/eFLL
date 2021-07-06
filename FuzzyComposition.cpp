886cd278712ac7dc0dd80019139b559f3b7b795e
AJ Alves <aj.alves@zerokol.com>
2021-07-06 09:46:06 -0300

    chore: improve calculate

--- FuzzyComposition.cpp
+++ FuzzyComposition.cpp
@@ -55,7 +55,6 @@ bool FuzzyComposition::addPoint(float point, float pertinence)
         {
             if (aux->next == NULL)
             {
-
                 // make the relations between them
                 newOne->previous = aux;
                 aux->next = newOne;
@@ -131,58 +130,56 @@ float FuzzyComposition::calculate()
     float numerator = 0.0;
     float denominator = 0.0;
     // while not in the end of the array, iterate
-    while (aux != NULL)
+    while (aux != NULL && aux->next != NULL)
     {
-        // float area;
-        // float middle;
-        //
-        // // if a singleton (Not properly a membership function)
-        // if ((aux->previous == NULL && aux->next == NULL) || aux->next == NULL)
-        // {
-        //     area = aux->pertinence;
-        //     middle = aux->point;
-        // }
-        // // else if a triangle (Not properly a membership function)
-        // else if (aux->pertinence == 0.0 || aux->next->pertinence == 0.0)
-        // {
-        //     float pertinence;
-        //     if (aux->pertinence > 0.0)
-        //     {
-        //         pertinence = aux->pertinence;
-        //     }
-        //     else
-        //     {
-        //         pertinence = aux->next->pertinence;
-        //     }
-        //     area = ((aux->next->point - aux->point) * pertinence) / 2.0;
-        //     if (aux->pertinence < aux->next->pertinence)
-        //     {
-        //         middle = ((aux->next->point - aux->point) / 1.5) + aux->point;
-        //     }
-        //     else
-        //     {
-        //         middle = ((aux->next->point - aux->point) / 3.0) + aux->point;
-        //     }
-        // }
-        // // else if a square (Not properly a membership function)
-        // else if ((aux->pertinence > 0.0 && aux->next->pertinence > 0.0) && (aux->pertinence == aux->next->pertinence))
-        // {
-        //     area = (aux->next->point - aux->point) * aux->pertinence;
-        //     middle = ((aux->next->point - aux->point) / 2.0) + aux->point;
-        // }
-        // // else if a trapeze (Not properly a membership function)
-        // else if ((aux->pertinence > 0.0 && aux->next->pertinence > 0.0) && (aux->pertinence != aux->next->pertinence))
-        // {
-        //     area = ((aux->pertinence + aux->next->pertinence) / 2.0) * (aux->next->point - aux->point);
-        //     middle = ((aux->next->point - aux->point) / 2.0) + aux->point;
-        // }
-        //
-        // numerator += middle * area;
-        // denominator += area;
-
-        numerator += aux->point * aux->pertinence;
-        denominator += aux->pertinence;
-
+        float area = 0.0;
+        float middle = 0.0;
+        // if it is a singleton
+        if (aux->pertinence != aux->next->pertinence && aux->point == aux->next->point)
+        {
+            // enter in all points of singleton, but calculate only once
+            if (aux->pertinence > 0.0)
+            {
+                area = aux->pertinence;
+                middle = aux->point;
+            }
+        }
+        // if a triangle (Not properly a membership function)
+        else if (aux->pertinence == 0.0 || aux->next->pertinence == 0.0)
+        {
+            float pertinence;
+            if (aux->pertinence > 0.0)
+            {
+                pertinence = aux->pertinence;
+            }
+            else
+            {
+                pertinence = aux->next->pertinence;
+            }
+            area = ((aux->next->point - aux->point) * pertinence) / 2.0;
+            if (aux->pertinence < aux->next->pertinence)
+            {
+                middle = ((aux->next->point - aux->point) / 1.5) + aux->point;
+            }
+            else
+            {
+                middle = ((aux->next->point - aux->point) / 3.0) + aux->point;
+            }
+        }
+        // else if a square (Not properly a membership function)
+        else if ((aux->pertinence > 0.0 && aux->next->pertinence > 0.0) && aux->pertinence == aux->next->pertinence)
+        {
+            area = (aux->next->point - aux->point) * aux->pertinence;
+            middle = ((aux->next->point - aux->point) / 2.0) + aux->point;
+        }
+        // else if a trapeze (Not properly a membership function)
+        else if ((aux->pertinence > 0.0 && aux->next->pertinence > 0.0) && aux->pertinence != aux->next->pertinence)
+        {
+            area = ((aux->pertinence + aux->next->pertinence) / 2.0) * (aux->next->point - aux->point);
+            middle = ((aux->next->point - aux->point) / 2.0) + aux->point;
+        }
+        numerator += middle * area;
+        denominator += area;
         aux = aux->next;
     }
     // avoiding zero division
